8cacacca8d080722b31a2f1787252d63ed7ed0de
hrcoop16 <hrcoop16@gmail.com>
2022-09-13 18:35:56 -0500

    employee test and class completed" "

--- Main/__tests__/Employee.test.js
+++ Main/__tests__/Employee.test.js
@@ -1,3 +1,20 @@
 const Employee = require("../lib/Employee");
 
-describe("Employee", () => {});
+describe("Employee", () => {
+  describe("Initialization", () => {
+    it("should create an object with a name, id, and email if provided valid arguments", () => {
+      const employee = new Employee("Employee1", 1, "employee1@gmail.com");
+
+      //verify that the new object has the correct properties
+      expect(employee.name).toEqual("Employee1");
+      expect(employee.id).toEqual(1);
+      expect(employee.email).toEqual("employee1@gmail.com");
+
+      //verify that fucntions can be called and return the correct properties
+      expect(employee.getName()).toEqual("Employee1");
+      expect(employee.getId()).toEqual(1);
+      expect(employee.getEmail()).toEqual("employee1@gmail.com");
+      expect(employee.getRole()).toEqual("Employee");
+    });
+  });
+});
