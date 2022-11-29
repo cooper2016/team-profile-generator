68714cf9278301775a8e08bf755490f5357f884f
hrcoop16 <hrcoop16@gmail.com>
2022-11-28 22:43:42 -0600

    added readme information

--- Main/index.js
+++ Main/index.js
@@ -1,3 +1,69 @@
 // include dependeancies
 const inquirer = require("inquirer");
 const fs = require("fs");
+const manager = require("./lib/Manager");
+const intern = require("./lib/Intern");
+const engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+
+//array of our team members
+const team = [];
+
+// questions that will be asked when the app starts about manager
+const managerQuestions = [
+  {
+    type: "input",
+    message: "What is your team managers name?",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "What is your team managers id",
+    name: "managerId",
+  },
+  {
+    type: "input",
+    message: "What is your team managers email?",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "What is your team managers office number?",
+    name: "managerPhone",
+  },
+];
+
+const continuePrompt = [
+  {
+    type: "checkbox",
+    message: "Would you like to add an Engineer, Intern, or no one?",
+    name: "addMember",
+  },
+];
+
+// initialization function
+const init = () => {
+  //ask client for team manager information
+  inquirer.prompt(managerQuestions).then((answer) => {
+    const manager = new Manager(
+      answer.managerName,
+      answer.managerId,
+      answer.managerEmail,
+      answer.managerPhone
+    );
+    team.push(manager);
+  });
+};
+
+const addAnotherMember = () => {};
+
+// Function to write out HTML File
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err
+      ? console.log(err)
+      : console.log("Successfully created index.html file!")
+  );
+}
+
+init();
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # team-profile-generator
+## Description 
+This program is a node.js command line applcaiton designed to take in information about employees on a software engineering team and generates an HTML webpage that displays a summary for each person. This program uses Javascript, node.js and uses the inquirer package. This application additionally uses tests to make sure the code is working correctly. 
+
+## Screenshot of Application
+
+![Screenshot of Application]()
+
+## Link to walk through
+
+[]()
